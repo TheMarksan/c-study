b7db6f9ff636565e7e1515e0d62c1e1bca4bd196
TheMarksan <marcosmelodossantos.com@gmail.com>
2023-06-17 11:59:54 -0300

    Função printF

--- 1. comments/comment.c
+++ 1. comments/comment.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(){
     /* Printando "Hello World" */
-    printf("Hello World");
+    printf("Hello World\n");
     return 0;
 }
---
+++ 2. printf-function/printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// caractere de escape quebra de linha -> \n
+
+int main()
+{
+    /* output function */
+    printf("\nValor retornado: %d\n", printf("Olá, mundo!"));
+    
+    return 0;
+}
