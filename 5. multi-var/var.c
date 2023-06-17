0d30fa75a7a058c2562596842da8c25bba6b7523
TheMarksan <marcosmelodossantos.com@gmail.com>
2023-06-17 16:53:35 -0300

    FIx

--- 5. multi-var/var.c
+++ 5. multi-var/var.c
@@ -4,14 +4,14 @@
 
 int main()
 {
-    char sexo;
+    char sex;
     int idade;
     double peso, altura;
 
     printf("Digite sexo(F ou M), idade, peso e altura:\n");
-    scanf("%c%d%lf%lf", &sexo, &idade, &peso, &altura);
+    scanf("%c%d%lf%lf", &sex, &idade, &peso, &altura);
 
-    printf("Sexo: %c\nIdade: %d\nPeso: %.1lf\nAltura: %.2lf\n", sexo, idade, peso, altura);
+    printf("Sexo: %c\nIdade: %d\nPeso: %.1lf\nAltura: %.2lf\n", sex, idade, peso, altura);
 
     return 0;
 }
